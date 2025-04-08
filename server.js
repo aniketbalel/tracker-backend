76878964b73a4ca2a805a5e3c44cfd79642b2341
aniketbalel <aniketbalel@gmail.com>
2025-04-09 04:49:11 +0530

    2ns

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); 
 
 // API endpoint to track number
 app.post('/api/track', async (req, res) => {
